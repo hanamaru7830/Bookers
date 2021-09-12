ec09c16118181c6bbbcd05d80ce837bc8822f04c
hanamaru7830 <xwsmilexw@gmail.com>
2021-09-12 05:44:57 +0000

    add controller,model

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+  
+  def create
+  end
+  
+  def updete
+  end
+  
+  def destroy
+  end
+
+  def edit
+  end
+end
