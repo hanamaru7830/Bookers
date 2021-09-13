630af36c20ad48a9c93e5a08afd8d82adf8cb158
hanamaru7830 <xwsmilexw@gmail.com>
2021-09-13 08:17:54 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,44 @@
 class BooksController < ApplicationController
-  def index
-  end
-
+  #@モデル名（:id）で1つののデータを持ってくるから
   def show
+    @book = Book.find(params[:id])
+  end
+  #@モデルの複数形（.all）で複数のデータを持ってくるから
+  #@モデル＝モデル.newで投稿を保存できる
+  def index
+    @books = Book.all
+    @book = Book.new
   end
   
   def create
+    # １. データを新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # ３. トップ画面へリダイレクト
+    redirect_to book_path(book.id)
   end
   
-  def updete
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
   
   def destroy
+    book = Book.find(params[:id])
+    
   end
 
-  def edit
+
+  
+  private
+  #ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
